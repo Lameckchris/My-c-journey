c3a46d56920ff254d35648387a8ce41c544d6b35
Chrispine Wameyo <98254121+Lameckchris@users.noreply.github.com>
2024-05-30 13:41:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//
+i//
 //  main.c
 //  C language 1
 //
@@ -7,8 +7,8 @@
 /*Shipping companies don't especially like boxes that are large but very light, since they take up valuable space in a truck or airplaine.Infact, companies often charge extra for such a box, basing the fee on its volume instead of its weight. In the United States the Usual method is to divide the volume by 166 (the allowed number of cubic inches per pound.) If this number - the box's "dimension" or "volumentric" weight-exceeds its actual weight, the shipping fee is based on dimensional weight.(The 166 divisor for international shipments and 194 divisor for domestic/local shipment)
 Here is a C program that is written to compute dimensional weight of the box */
 #include <stdio.h>
-#define Local_divisor 194;
-#define International_divisor 166;
+#define Local_divisor 194
+#define International_divisor 166
 //program to compute the dimension weight of a box.
 float height;
 float length;
